866df7302fd082f660b88e816ecfb562c13fdf5d
Andrew Harrison <aharrison92@gmail.com>
2018-10-20 12:06:05 +0800

    cleaned up user interface and added extra calculation

--- InterestCalculator/ClientApp/src/App.js
+++ InterestCalculator/ClientApp/src/App.js
@@ -5,15 +5,34 @@ export default class App extends Component {
 
   render() {
     return (
-          <div>
+          <div class="col-md-12">
               <h1>Loan Calculator</h1>
             <p>Enter your values in the form below and hit the Calculate button to determine your monthly loan repayments</p>
-            <form name="Calculate" action="api/data/Calculate" method="post">
-                <input name="LoanValue" type="Number"/>
+            <form name="Calculate" action="api/data/CalculateRepayment" method="post">
+                <label class="col-md-3" for="LoanValue">Loan Amount (dollars)</label>
+                <input name="LoanValue" type="Number" />
+                <br />
+                <label class="col-md-3" for="InterestRate">Interest Rate (percentage)</label>
                 <input name="InterestRate" type="Number" />
+                <br />
+                <label class="col-md-3" for="Term">Loan Term (years)</label>
                 <input name="Term" type="Number" />
-
+                <br />
                 <input name="Submit" type="submit" value="Calculate"/>
+            </form>
+            <br />
+            <p>Enter your values in the form below and hit the Calculate button to determine the total value of your loan</p>
+            <form name="Calculate" action="api/data/CalculateCurrentValue" method="post">
+                <label class="col-md-3" for="Repayment"> Monthly Repayment (dollars)</label>
+                <input name="Repayment" type="Number" />
+                <br />
+                <label class="col-md-3" for="InterestRate">Interest Rate (percentage)</label>
+                <input name="InterestRate" type="Number" />
+                <br />
+                <label class="col-md-3" for="Term">Loan Term (years)</label>
+                <input name="Term" type="Number" />
+                <br />
+                <input name="Submit" type="submit" value="Calculate" />
             </form>
 
             
--- InterestCalculator/Controllers/DataController.cs
+++ InterestCalculator/Controllers/DataController.cs
@@ -10,11 +10,19 @@ namespace InterestCalculator.Controllers
     public class DataController : Controller
     {
         [HttpPost("[action]")]
-        public double Calculate(double LoanValue, double InterestRate, double Term)
+        public double CalculateRepayment(double LoanValue, double InterestRate, double Term)
         {
             double interest = InterestRate/100;
             double repayment = LoanValue / ((1 - (1 / Math.Pow((1 + (interest / 12)), (Term * 12)))) / interest / 12);
             return repayment;
+        }
+
+        [HttpPost("[action]")]
+        public double CalculateCurrentValue(double Repayment, double InterestRate, double Term)
+        {
+            double interest = InterestRate / 100;
+            double currentValue = Repayment * (1 - Math.Pow((1 + (interest/12)),(-1 * Term))) / (interest/12);
+            return currentValue;
         }
     }
 }
